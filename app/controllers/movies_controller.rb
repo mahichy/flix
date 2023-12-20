49ffdf831f3fe7a09ce0a6dfc82da442ea4d2628
mahichy <mohiuddinoshim@yahoo.com>
2023-12-20 12:14:10 +0600

    creating records

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -16,11 +16,27 @@ class MoviesController < ApplicationController
     @movie = Movie.find(params[:id])
 
     # @movie = Movie.update(params[:movie])
-    movie_params =
-      params.require(:movie).
-        permit(:title, :description, :rating, :relesed_on, :total_gross)
+      
 
           @movie.update(movie_params)
           redirect_to @movie
+  end
+
+  def new
+    @movie = Movie.new
+  end
+
+  def create
+    @movie = Movie.new(movie_params)
+    @movie.save
+    redirect_to @movie
+  end
+
+
+  private
+
+  def movie_params
+    params.require(:movie).
+        permit(:title, :description, :rating, :relesed_on, :total_gross)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,12 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   root 'movies#index'
-  get 'movies' => "movies#index"
-  get 'movies/:id' => "movies#show", as: 'movie'
-  get 'movies/:id/edit' => 'movies#edit', as: "edit_movie"
-  patch 'movies/:id' => 'movies#update'
+
+  # get 'movies' => "movies#index"
+  # get 'movies/new' => 'movies#new'
+  # get 'movies/:id' => "movies#show", as: 'movie'
+  # get 'movies/:id/edit' => 'movies#edit', as: "edit_movie"
+  # patch 'movies/:id' => 'movies#update'
+
+  resources :movies
 end
