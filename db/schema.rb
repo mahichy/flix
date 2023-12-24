914c445f4a96a4cace498b2cd5a40008fdd93321
mahichy <mohiuddinoshim@yahoo.com>
2023-12-24 09:57:49 +0600

    one to many:belongs_to

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_12_20_132548) do
+ActiveRecord::Schema[7.0].define(version: 2023_12_24_034538) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -27,4 +27,15 @@ ActiveRecord::Schema[7.0].define(version: 2023_12_20_132548) do
     t.string "image_file_name", default: "placeholder.png"
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.string "name"
+    t.integer "stars"
+    t.text "comment"
+    t.bigint "movie_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["movie_id"], name: "index_reviews_on_movie_id"
+  end
+
+  add_foreign_key "reviews", "movies"
 end
