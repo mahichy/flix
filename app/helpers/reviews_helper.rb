bc4d0d9c82853e2e5814fc65fa0c5d6f2aefe008
mahichy <mohiuddinoshim@yahoo.com>
2023-12-27 14:01:10 +0600

    One to many logic

--- app/helpers/reviews_helper.rb
+++ app/helpers/reviews_helper.rb
@@ -1,2 +1,12 @@
 module ReviewsHelper
+
+  def average_stars(movie)
+    if movie.average_stars.zero?
+      content_tag(:strong, "No review")
+    else
+      # pluralize(movie.average_stars, "star")
+      pluralize(number_with_precision(movie.average_stars, precision: 1) , "star")
+
+    end
+  end
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -20,4 +20,8 @@ class Movie < ApplicationRecord
   def flop?
     total_gross == 0 || total_gross < 255000000
   end
+
+  def average_stars
+    reviews.average(:stars) || 0.0
+  end
 end
