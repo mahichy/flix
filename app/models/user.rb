306c5314c7ba54d350ce3e8d53875c92b78ce8ed
mahichy <mohiuddinoshim@yahoo.com>
2023-12-28 15:01:12 +0600

    edit user account

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,9 @@ class User < ApplicationRecord
   validates :email, format: { with: /\S+@\S+/ },
             uniqueness: {case_sensative: false}
             validates :password, length: { minimum: 6, allow_blank: true }
+            validates :username, presence: true,
+            format: { with: /\A[A-Z0-9]+\z/i },
+            uniqueness: { case_sensitive: false }
 
 
 
