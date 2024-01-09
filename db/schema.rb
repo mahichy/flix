b2f5e9585f74c47c3b785273a23d99324dfb9793
mahichy <mohiuddinoshim@yahoo.com>
2024-01-09 15:43:42 +0600

    Many to many with chackboxes 1

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_01_08_054932) do
+ActiveRecord::Schema[7.0].define(version: 2024_01_09_091603) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "characterizations", force: :cascade do |t|
+    t.bigint "movie_id", null: false
+    t.bigint "genre_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["genre_id"], name: "index_characterizations_on_genre_id"
+    t.index ["movie_id"], name: "index_characterizations_on_movie_id"
+  end
 
   create_table "favorites", force: :cascade do |t|
     t.bigint "movie_id", null: false
@@ -21,6 +30,12 @@ ActiveRecord::Schema[7.0].define(version: 2024_01_08_054932) do
     t.datetime "updated_at", null: false
     t.index ["movie_id"], name: "index_favorites_on_movie_id"
     t.index ["user_id"], name: "index_favorites_on_user_id"
+  end
+
+  create_table "genres", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "movies", force: :cascade do |t|
@@ -56,6 +71,8 @@ ActiveRecord::Schema[7.0].define(version: 2024_01_08_054932) do
     t.boolean "admin", default: false
   end
 
+  add_foreign_key "characterizations", "genres"
+  add_foreign_key "characterizations", "movies"
   add_foreign_key "favorites", "movies"
   add_foreign_key "favorites", "users"
   add_foreign_key "reviews", "movies"
